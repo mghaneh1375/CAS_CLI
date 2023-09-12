cb1b3c4d81a08d4b2a9e8b0baf8638d1ef49d849
mghaneh1375 <mghaneh1375@gmail.com>
2023-09-12 15:00:18 +0330

    callback modified. (UUID class added.)

--- src/main/java/bogen/studio/cas_client/Controller/LoginController.java
+++ src/main/java/bogen/studio/cas_client/Controller/LoginController.java
@@ -1,89 +1,60 @@
 package bogen.studio.cas_client.Controller;
 
-import bogen.studio.cas_client.DTO.LoginRequest;
-import bogen.studio.cas_client.Security.SecurityProperties;
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-import org.apache.commons.io.IOUtils;
 import org.json.JSONObject;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import java.security.KeyFactory;
 import java.security.NoSuchAlgorithmException;
-import java.security.Principal;
 import java.security.PublicKey;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.X509EncodedKeySpec;
-import java.util.Base64;
-import java.util.HashMap;
-
-import static java.nio.charset.StandardCharsets.UTF_8;
+import java.util.*;
 
 @RestController
 public class LoginController {
 
-    @Autowired
-    SecurityProperties securityProperties;
+    @Value("${cas.address}")
+    String CAS_SERVER;
 
+    @Value("${cas.clientId}")
+    String CLIENT_ID;
 
-    private String getPublicKeyAsString() {
-        try {
-            return IOUtils.toString(securityProperties.getJwt().getPublicKey().getInputStream(), UTF_8);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+    @Value("${cas.password}")
+    String PASSWORD;
+
+    @Value("${cas.public-key}")
+    String PUB_KEY;
+
+    private static final String[] DONT_ALLOW_SIGN_UP = new String[] {
+            "boom.bogenstudio.com"
+    };
+
+    class UUID {
+
+        String uuid;
+        String token;
+        long expireAt;
+        long lastGet;
+
+        public UUID(String uuid, String token, long expireAt) {
+            this.uuid = uuid;
+            this.token = token;
+            this.expireAt = expireAt;
+            lastGet = System.currentTimeMillis();
         }
     }
 
-    @GetMapping("/user")
-    @ResponseBody
-    public String user(HttpServletRequest request, Authentication authentication, Principal principal) {
-
-        String token = request.getHeader("Authorization");
-        token = token.replace("Bearer ", "");
-
-        String pubkeyb64 = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAtx5XIQ7QRnKZRRDexf7X" +
-                "zZxMhf+hE807qwi0Ul1WWcLt5be7zsHGdOsn3BGGB8BAmeA54qespU7MJFNIW21l" +
-                "Qb/XqexShrsiOvVxs8Z75RZfA2UjYwV1tHW58MTIgRdER67aJj0hIofgOFztB0CN" +
-                "RHaehltR3up3tEPnz0HxsuSESmPccU86YJUKyu2QUW7hcrj0yUBeFiFrDhRKel5O" +
-                "9+X862FOE+aSWAaX69hTUTf8CDSXpAlH93xX27Uz5h/bTbSIB2fXbsINe0d4HdX2" +
-                "TQceyBQe+LoNmIfrnTPjyvf67ICGYFkCH8G7zF9851o63sbquWKA6NQ90ydkV/hO" +
-                "twIDAQAB";
-
-        byte[] pubkeyder = Base64.getDecoder().decode(pubkeyb64);
-        PublicKey pubkey = null;
-        try {
-            pubkey = KeyFactory.getInstance("RSA").generatePublic(new X509EncodedKeySpec(pubkeyder));
-
-            Claims claims = Jwts.parser().setSigningKey(pubkey).parseClaimsJws(token).getBody();
-            System.out.println(claims.getExpiration());
-            System.out.println(claims.getId());
-
-            System.out.println(principal.getName());
-            System.out.println(principal);
-
-            authentication.getAuthorities().forEach(grantedAuthority -> {
-                System.out.println(grantedAuthority.getAuthority());
-            });
-
-        } catch (InvalidKeySpecException e) {
-            throw new RuntimeException(e);
-        } catch (NoSuchAlgorithmException e) {
-            throw new RuntimeException(e);
-        }
-
-        return token;
-    }
-
+    private static ArrayList<UUID> uuids = new ArrayList<>();
 
     @GetMapping("/login")
     public ModelAndView get(HttpServletRequest request,
@@ -95,6 +66,33 @@ public class LoginController {
         Object token = request.getSession().getAttribute("token");
 
         if (token != null) {
+
+            UUID uuid = uuids.stream().filter(e -> e.token.equals(token)).findFirst().orElse(null);
+
+            if(uuid != null && System.currentTimeMillis() - uuid.lastGet > 10000) {
+
+                HttpResponse<JsonNode> res;
+                try {
+                    res = Unirest.post(callback)
+                            .header("Content-Type", "application/json")
+                            .body(new JSONObject()
+                                    .put("token", token)
+                            ).asJson();
+
+                    if (res.getStatus() == 200) {
+                        String newUuid = res.getBody().getObject().getString("data");
+                        uuid.lastGet = System.currentTimeMillis();
+                        uuid.uuid = newUuid;
+
+                        response.setHeader("Location", redirectUrl + "?uuid=" + newUuid);
+                        response.setStatus(302);
+                        return null;
+                    }
+
+                } catch (UnirestException ignore) {}
+
+            }
+
             response.setHeader("Location", redirectUrl);
             response.setStatus(302);
             return null;
@@ -103,8 +101,14 @@ public class LoginController {
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.addObject("callback", callback);
         modelAndView.addObject("redirectUrl", redirectUrl);
+        modelAndView.addObject("allowSignUp", Arrays.stream(DONT_ALLOW_SIGN_UP).noneMatch(callback::contains));
         modelAndView.setViewName("login");
         return modelAndView;
+    }
+
+    @GetMapping("/logout")
+    public void logout(HttpServletRequest request) {
+        request.getSession().removeAttribute("token");
     }
 
     @PostMapping("/login")
@@ -118,15 +122,23 @@ public class LoginController {
 
         try {
 
-            HttpResponse<JsonNode> res = Unirest.post("http://193.151.137.75:9000/oauth/token")
+            HttpResponse<JsonNode> res = Unirest.post(CAS_SERVER)
                     .queryString("grant_type", "password")
                     .queryString("username", username)
                     .queryString("password", password)
-                    .basicAuth("clientId", "Admin123").asJson();
+                    .basicAuth(CLIENT_ID, PASSWORD).asJson();
 
             System.out.println(res.getStatus());
 
             if (res.getStatus() != 200) {
+                ModelAndView modelAndView = new ModelAndView();
+                modelAndView.addObject("callback", callback);
+                modelAndView.addObject("redirectUrl", redirectUrl);
+                modelAndView.addObject("error", "نام کاربری و یا رمزعبور وارد شده استباه است.");
+                modelAndView.setViewName("login");
+
+                response.setHeader("Location", "http://" + request.getHeader("host") + "/login?redirectUrl=" + redirectUrl + "&callback=" + callback + "&error");
+                response.setStatus(302);
                 return;
             }
 
@@ -146,12 +158,27 @@ public class LoginController {
                 return;
             }
 
-            response.setHeader("Location", redirectUrl + "?uuid=" + res.getBody().getObject().getString("data"));
-            response.setStatus(302);
+            byte[] pubkeyder = Base64.getDecoder().decode(PUB_KEY);
+            PublicKey pubkey;
+            try {
+                pubkey = KeyFactory.getInstance("RSA").generatePublic(new X509EncodedKeySpec(pubkeyder));
+                Claims claims = Jwts.parser().setSigningKey(pubkey).parseClaimsJws(token).getBody();
+
+                String uuid = res.getBody().getObject().getString("data");
+                uuids.add(new UUID(uuid, token, claims.getExpiration().getTime()));
+
+                response.setHeader("Location", redirectUrl + "?uuid=" + uuid);
+                response.setStatus(302);
+
+            } catch (InvalidKeySpecException | NoSuchAlgorithmException e) {
+                throw new RuntimeException(e);
+            }
+
 
         } catch (UnirestException e) {
             e.printStackTrace();
         }
+
     }
 
 }
