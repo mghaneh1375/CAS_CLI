bdd1b6c60a6ffcc5cbfbbbdb7ed42fbbce8b7470
mghaneh1375 <mghaneh1375@gmail.com>
2023-10-19 23:20:01 +0330

    nothing

--- src/main/java/bogen/studio/cas_client/Security/SecurityConfig.java
+++ src/main/java/bogen/studio/cas_client/Security/SecurityConfig.java
@@ -45,7 +45,7 @@ public class SecurityConfig extends ResourceServerConfigurerAdapter {
 
         http = http.csrf().disable();
         http = http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.ALWAYS).and();
-
+        
         http
                 .authorizeRequests()
                 .antMatchers("/cas/**").permitAll()
@@ -56,7 +56,8 @@ public class SecurityConfig extends ResourceServerConfigurerAdapter {
                 .antMatchers("/static/**", "/favicon.ico").permitAll()
                 .anyRequest().authenticated()
                 .and()
-                .headers().frameOptions().sameOrigin();
+                .headers().frameOptions().sameOrigin()
+                .httpStrictTransportSecurity().disable();
     }
 
     @Bean
